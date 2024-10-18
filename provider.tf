0c33ef05584c0f314e3a94b0e578b8b8ef9e4881
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-10-18 17:35:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
 provider "aws" {
 region     = "us-east-1"
 }
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}

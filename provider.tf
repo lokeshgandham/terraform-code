b68951d4d726f20989fbb99642108ca62758020c
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-10-18 15:48:39 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region     = "us-east-1"
+}
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}

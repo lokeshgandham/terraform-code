6de46377ec2aeb0b325cbda10fd431bba5678575
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-11-05 22:33:55 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sudo yum -y install git
-sudo yum -y install docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
-sudo chmod 666 /var/run/docker.sock
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/Jyothirmai-503/project-2.git
-cd project-2/
-docker-compose up -d
+sudo yum -y update
+sudo yum install -y git
+sudo yum install -y python3-pip
+git clone https://github.com/Jyothirmai-503/indian-liver-patients.git
+cd indian-liver-patients/
+pip3 install -r requirements.txt
+python3 app.py

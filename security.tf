60e1d1105b8dd3e0f756eb5e43773f2049668971
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-11-05 22:20:52 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,35 @@
+
+#creating security group
+resource "aws_security_group" "security1" {
+  vpc_id = aws_vpc.demovpc.id
+
+#inbound Rules
+ingress  {
+    from_port = "7074"
+    to_port = "7074"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ingress  {
+    from_port = "8000"
+    to_port = "8000"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ingress  {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+}
+tags = {
+  Name = "terraform sg"
+}
+}

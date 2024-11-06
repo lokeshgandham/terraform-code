e1252d36be0ed278305e204cf1a6be9707e92094
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-11-06 19:10:43 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,7 +2,7 @@
 sudo yum -y update
 sudo yum install -y git
 sudo yum install -y python3-pip
-git clone https://github.com/Jyothirmai-503/indian-liver-patients.git
-cd indian-liver-patients/
+git clone https://github.com/Jyothirmai-503/MyFuel.git
+cd MyFuel/
 pip3 install -r requirements.txt
 python3 app.py

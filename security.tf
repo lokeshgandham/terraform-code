0028630ab9cde2d5e0f83253edb4fb75c2b53950
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-11-06 21:43:30 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -17,12 +17,6 @@ ingress  {
     cidr_blocks = ["0.0.0.0/0"]
 }
 ingress  {
-    from_port = "80"
-    to_port = "80"
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-}
-ingress  {
     from_port = 22
     to_port = 22
     protocol = "tcp"

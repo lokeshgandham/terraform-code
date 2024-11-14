0e5158ae03253ad221f69b0e10a0653f3852ec93
Jyothirmai-503 <jyothialuvoji@gmail.com>
2024-11-14 15:48:50 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,6 +3,9 @@ sudo yum -y update
 sudo yum install -y git
 sudo yum install -y python3-pip
 git clone https://github.com/Jyothirmai-503/MyFuel.git
+cd /
+sudo mv MyFuel/ /home/ec2-user/
+cd /home/ec2-user/
 cd MyFuel/
 pip3 install -r requirements.txt
 python3 app.py
